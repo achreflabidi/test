61322a4f04ece8494b59cf64469f50b0d8727476
achreflabidi <abidi.achref93@live.fr>
2019-04-25 16:03:13 +0100

    first commit

---
+++ my-app/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { DashbordComponent } from './dashbord/dashbord.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    RegisterComponent,
+    LoginComponent,
+    DashbordComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
